c262cba38da7297d72425ad2938c7d53b860acea
pedrobrito07 <61784441+pedrobrito07@users.noreply.github.com>
2025-08-24 18:38:33 -0300

    secondd commit

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 // PIN do admin
-const ADMIN_PIN = "CoxinhadeChocolate";
+const ADMIN_PIN = "TentaAgoraViado";
 
 // Lista de times
 const times = ["Bird","Bravus","Fire","Ice","Sand","Speed","Stone","Storm","Taurus","Tiger"];
